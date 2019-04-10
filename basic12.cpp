11e6a843c8f545cacba9ec6e8e661c137df6a453
jorden134677 <48340565+jorden134677@users.noreply.github.com>
2019-04-10 23:42:48 +0800

    Add files via upload

---
+++ basic12.cpp
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h> 
+
+int main(void)
+{
+    int n;     
+    int i, j, k, m; 
+    printf("輸入正方形的邊長：\n");
+    scanf("%d", &n); 
+    
+    m = n;
+    for(k=1 ; k<=m ; k++){
+       for(j=n ; j<m ; j++)
+          printf(" ");
+               for(i = 1 ; i <= n ; i++ )
+                  printf("*");
+                  printf("\n");
+                  n--;
+    }
+    
+    system("pause");
+    return 0;
+}
